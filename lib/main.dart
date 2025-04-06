b462484e3065d1ff3494c592f7b96fcde793cd46
Nikhil Khatri <nikhil@jioflight.com>
2025-04-06 11:04:25 +0545

    chages

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'This is a Mod Baby',
+              // 'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
